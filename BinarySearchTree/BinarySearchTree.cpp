7a6af6aeb2803554b70a246d8b4473e8853bce27
hi-lufz1 <latifufauzi@gmail.com>
2023-06-29 19:55:01 +0700

    11

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -81,9 +81,21 @@ public:
 			cout << ptr->info << " ";
 			inorder(ptr->rightchild);
 		}
-
 	}
 
+	void preorder(Node* ptr)
+	{
+		if (ROOT == NULL)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			
+		}
+	}
 
 
 };
